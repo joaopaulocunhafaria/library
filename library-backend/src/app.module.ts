1ed09e5f143277cd286535808f25054fcf482f76
joaopaulocunhafaria <joaopaulofaria98@gmail.com>
2024-12-02 17:54:07 -0300

    feat: Importing AuthorsModule on AppModule

--- library-backend/src/app.module.ts
+++ library-backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config'; 
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthorsModule } from './authors/authors.module';
 
 @Module({
   imports: [
@@ -15,7 +16,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: process.env.DATABASE_URL,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],  
       synchronize: true, 
-    })
+    }),
+    AuthorsModule
   ],
   controllers: [AppController],
   providers: [AppService],
