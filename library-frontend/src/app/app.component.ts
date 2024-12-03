8ff2eb3a58d7156e897c33a6c53cf2ba6143ce4f
joaopaulocunhafaria <joaopaulofaria98@gmail.com>
2024-12-02 23:17:38 -0300

    feat: Creating routing enviroment

--- library-frontend/src/app/app.component.ts
+++ library-frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { AuthorsComponent } from "./authors/authors.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AuthorsComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- library-frontend/src/app/app.routes.ts
+++ library-frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorDetailComponent } from './author-detail/author-detail.component';
+import { CreateAuthorComponent } from './create-author/create-author.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+    { path: '', component: AuthorsComponent },
+    { path: 'author/:id', component: AuthorDetailComponent },
+    { path: 'create-author', component: CreateAuthorComponent },
+  ];
