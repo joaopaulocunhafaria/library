51318c7e628f5fed9c915086f467b495ad949a45
joaopaulocunhafaria <joaopaulofaria98@gmail.com>
2024-12-03 01:40:57 -0300

    feat:Created update's routes

--- library-frontend/src/app/app.routes.ts
+++ library-frontend/src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { BooksListComponent } from './books/books-list/books-list.component';
 import { BooksDetailsComponent } from './books/books-details/books-details.component';
 import { CreateBookComponent } from './books/create-book/create-book.component';
 import { HomeComponent } from './home/home.component';
+import { UpdateAuthorComponent } from './author/update-author/update-author.component';
+import { BookUpdateComponent } from './books/book-update/book-update.component';
 
 export const appRoutes: Routes = [
     { path: '', component: HomeComponent     },
@@ -15,5 +17,7 @@ export const appRoutes: Routes = [
     { path: 'books', component: BooksListComponent },
     { path: 'books/:id', component: BooksDetailsComponent },
     { path: 'create-book', component: CreateBookComponent },
+    { path: 'update-author/:id', component: UpdateAuthorComponent },
+    { path: 'update-book/:id', component: BookUpdateComponent },
     
   ];
