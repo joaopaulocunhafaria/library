3fa6bc5d45bddf8c3a50e4e63fafe865a517b428
joaopaulocunhafaria <joaopaulofaria98@gmail.com>
2024-12-02 23:18:31 -0300

    feat: create author component

---
+++ library-frontend/src/app/create-author/create-author.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-author',
+  standalone: true,
+  imports: [FormsModule, RouterModule],
+  templateUrl: './create-author.component.html',
+  styleUrls: ['./create-author.component.scss'],
+})
+export class CreateAuthorComponent {
+  author = {
+    name: '',
+    birthDate: ''
+  };
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  // Enviar requisição POST para criar um novo autor
+  createAuthor() {
+    const apiUrl = 'http://localhost:3000/authors';
+     // Substitua com sua URL de API
+    this.http.post(apiUrl, this.author).subscribe({
+      next: () => {
+        alert('Author created successfully!');
+        this.router.navigate(['/']); // Redireciona para a página de autores após a criação
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+        alert('Failed to create author!');
+      }
+    });
+  }
+}
